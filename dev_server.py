affc007f9df1c0090ce989193e7d667739dc1e2b
Ettore Forigo <etty.2000.ef@gmail.com>
2017-05-15 22:11:08 +0200

    Added dev server

---
+++ dev_server.py
@@ -0,0 +1,22 @@
+import threading
+import queue
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def handle_data():
+    if request.method == 'POST':
+        for field in request.form:
+            if field.startswith('send_'):
+                print("RECEIVED FROM WEBPAGE:", field.split("send_")[1])
+    return render_template('index.html')
+
+
+def main():
+    app.run(host='0.0.0.0', port='80')
+
+if __name__ == "__main__":
+    main()
